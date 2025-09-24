76f4b39c2e32a55d23c3e8e41db518ef47257f26
vikash1063 <96657674+vikash1063@users.noreply.github.com>
2025-09-24 10:20:15 +0530

    Add files via upload

---
+++ CMakeFiles/library.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vikash/Documents/placementProjects/LibEase/src/add_book.cpp" "CMakeFiles/library.dir/src/add_book.cpp.o" "gcc" "CMakeFiles/library.dir/src/add_book.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/auth.cpp" "CMakeFiles/library.dir/src/auth.cpp.o" "gcc" "CMakeFiles/library.dir/src/auth.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/display_book.cpp" "CMakeFiles/library.dir/src/display_book.cpp.o" "gcc" "CMakeFiles/library.dir/src/display_book.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/display_members.cpp" "CMakeFiles/library.dir/src/display_members.cpp.o" "gcc" "CMakeFiles/library.dir/src/display_members.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/edit_profile.cpp" "CMakeFiles/library.dir/src/edit_profile.cpp.o" "gcc" "CMakeFiles/library.dir/src/edit_profile.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/issue_book.cpp" "CMakeFiles/library.dir/src/issue_book.cpp.o" "gcc" "CMakeFiles/library.dir/src/issue_book.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/main.cpp" "CMakeFiles/library.dir/src/main.cpp.o" "gcc" "CMakeFiles/library.dir/src/main.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/return_book.cpp" "CMakeFiles/library.dir/src/return_book.cpp.o" "gcc" "CMakeFiles/library.dir/src/return_book.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/search_book.cpp" "CMakeFiles/library.dir/src/search_book.cpp.o" "gcc" "CMakeFiles/library.dir/src/search_book.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/search_member.cpp" "CMakeFiles/library.dir/src/search_member.cpp.o" "gcc" "CMakeFiles/library.dir/src/search_member.cpp.o.d"
+  "/home/vikash/Documents/placementProjects/LibEase/src/utils.cpp" "CMakeFiles/library.dir/src/utils.cpp.o" "gcc" "CMakeFiles/library.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/library.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/library.dir/src/add_book.cpp.o"
+  "CMakeFiles/library.dir/src/add_book.cpp.o.d"
+  "CMakeFiles/library.dir/src/auth.cpp.o"
+  "CMakeFiles/library.dir/src/auth.cpp.o.d"
+  "CMakeFiles/library.dir/src/display_book.cpp.o"
+  "CMakeFiles/library.dir/src/display_book.cpp.o.d"
+  "CMakeFiles/library.dir/src/display_members.cpp.o"
+  "CMakeFiles/library.dir/src/display_members.cpp.o.d"
+  "CMakeFiles/library.dir/src/edit_profile.cpp.o"
+  "CMakeFiles/library.dir/src/edit_profile.cpp.o.d"
+  "CMakeFiles/library.dir/src/issue_book.cpp.o"
+  "CMakeFiles/library.dir/src/issue_book.cpp.o.d"
+  "CMakeFiles/library.dir/src/main.cpp.o"
+  "CMakeFiles/library.dir/src/main.cpp.o.d"
+  "CMakeFiles/library.dir/src/return_book.cpp.o"
+  "CMakeFiles/library.dir/src/return_book.cpp.o.d"
+  "CMakeFiles/library.dir/src/search_book.cpp.o"
+  "CMakeFiles/library.dir/src/search_book.cpp.o.d"
+  "CMakeFiles/library.dir/src/search_member.cpp.o"
+  "CMakeFiles/library.dir/src/search_member.cpp.o.d"
+  "CMakeFiles/library.dir/src/utils.cpp.o"
+  "CMakeFiles/library.dir/src/utils.cpp.o.d"
+  "library"
+  "library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
